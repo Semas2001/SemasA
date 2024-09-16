428fd8a4ca21eff0d2b2a2e95ebba006016b855f
Semas2001 <semasarmonaitis@gmail.com>
2024-09-16 14:41:17 +0100

    Creaetd the wholde design and back-end of the contacts page

--- contacts_script.js
+++ contacts_script.js
@@ -2,3 +2,34 @@ document.addEventListener("DOMContentLoaded", function () {
     const header = document.querySelector("#header");
     header.style.top = "0";
     header.style.transition = "top 0";});
+
+
+document.querySelector('form').addEventListener('submit', function(event) {
+event.preventDefault();
+
+
+const formData = new FormData(event.target);
+
+fetch(event.target.action, {
+    method: 'POST',
+    body: formData,
+    headers: {
+    'Accept': 'application/json'
+    }
+})
+.then(response => {
+    if (!response.ok) {
+    throw new Error('Failed to submit the form');
+    }
+
+    return response.json();
+})
+.then(() => {
+    window.location.href = './thanks.html';
+})
+.catch(error => {
+    console.error('Error:', error);
+
+});
+});
+
